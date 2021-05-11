b691db63f91369079899faae1c62b7ef1f50e55c
liupai <2319492349@qq.com>
2021-05-11 17:35:56 +0800

    ss

--- ruoyi-traces/src/main/java/com/ruoyi/traces/service/RetailerService.java
+++
@@ -1,7 +0,0 @@
-package com.ruoyi.traces.service;
-
-import com.ruoyi.traces.domain.TraceTransport;
-
-public interface RetailerService {
-    int updateReceiveStatus(TraceTransport traceTransport);
-}
