275f9a7309de0e94374c74678da713fe3875fe73
steakliu <2319492349@qq.com>
2021-05-11 23:29:23 +0800

    update blockchain-trace-applets/src/main.js.

--- blockchain-trace-applets/src/main.js
+++ blockchain-trace-applets/src/main.js
@@ -6,7 +6,7 @@ App.mpType = 'app'
 var Fly=require("flyio/dist/npm/wx") 
 var fly=new Fly
 Vue.prototype.$httpBlock = fly
-Vue.prototype.$httpUrl = "http://116.198.160.39:8080/route";
+Vue.prototype.$httpUrl = "http://ip:8080/route";
 
 const app = new Vue(App)
 app.$mount()
