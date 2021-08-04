a20eed7a7983a6ac07fb297d7fa65bfe899eaf7b
steakliu <2319492349@qq.com>
2021-08-04 09:25:09 +0000

    add blockchain-trace-pc/src/main.js. pc main.js文件

---
+++ blockchain-trace-pc/src/main.js
@@ -0,0 +1,96 @@
+import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './assets/styles/element-variables.scss'
+
+import '@/assets/styles/index.scss' // global css
+import '@/assets/styles/ruoyi.scss' // ruoyi css
+import App from './App'
+import store from './store'
+import router from './router'
+import permission from './directive/permission'
+
+import './assets/icons' // icon
+import './permission' // permission control
+import { getDicts } from "@/api/system/dict/data";
+import { getConfigKey } from "@/api/system/config";
+import { parseTime, resetForm, addDateRange, selectDictLabel, selectDictLabels, download, handleTree } from "@/utils/ruoyi";
+import Pagination from "@/components/Pagination";
+//自定义表格工具扩展
+import RightToolbar from "@/components/RightToolbar"
+import axios from 'axios'
+import SnowflakeId from "snowflake-id";
+
+import JsonViewer from 'vue-json-viewer'
+ 
+// Import JsonViewer as a Vue.js plugin
+Vue.use(JsonViewer)
+
+// 全局方法挂载
+Vue.prototype.getDicts = getDicts
+Vue.prototype.getConfigKey = getConfigKey
+Vue.prototype.parseTime = parseTime
+Vue.prototype.resetForm = resetForm
+Vue.prototype.addDateRange = addDateRange
+Vue.prototype.selectDictLabel = selectDictLabel
+Vue.prototype.selectDictLabels = selectDictLabels
+Vue.prototype.download = download
+Vue.prototype.handleTree = handleTree
+
+Vue.prototype.msgSuccess = function (msg) {
+  this.$message({ showClose: true, message: msg, type: "success" });
+}
+
+Vue.prototype.msgError = function (msg) {
+  this.$message({ showClose: true, message: msg, type: "error" });
+}
+
+Vue.prototype.msgInfo = function (msg) {
+  this.$message.info(msg);
+}
+
+Vue.prototype.$httpBlock = axios;
+Vue.prototype.$httpUrl = "http://localhost:8080/route";
+Vue.prototype.$snowFlakeId = SnowflakeId;
+Vue.prototype.$store = store;
+// 全局组件挂载
+Vue.component('Pagination', Pagination)
+Vue.component('RightToolbar', RightToolbar)
+
+Vue.use(permission)
+
+//引入地图插件
+import VueAMap from 'vue-amap';
+Vue.use(VueAMap);
+ 
+//高德地图方法
+VueAMap.initAMapApiLoader({
+　　key: '3e5db00ff818adf2b2e2f600f0a72608',
+　　　v: '1.4.4'
+});
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
+ */
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
